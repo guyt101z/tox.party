fe98da18e0fea69191af0b67ca0073c5a096a1f6
installgen2 <gen2@kek.club>
2015-07-29 23:26:49 +0100

    clean up some junk, make it actually work, add saving, add logging, and probably some more

--- index.js
+++ index.js
@@ -2,52 +2,83 @@
 var named = require("node-named");
 var dnsServer = named.createServer();
 
-// Frontend stuff
+// Webapp stuff
 var bodyParser = require("body-parser");
 var express = require("express");
 var webapp = express();
-var server = webapp.listen(3000, console.log("Webapp running..."));
 
+webapp.listen(3000, console.log("Webapp running..."));
 webapp.use(bodyParser.json());
-webapp.use(bodyParser.urlencoded({ extended: true }));
+webapp.use(bodyParser.urlencoded({extended: true}));
 webapp.use(express.static("www"));
 
 // Other stuff
-var toxRecords = {};
+var fs = require("fs");
+
+// Startup
+fs.readFile("./toxRecords.json", function(err, data) {
+	if (!err) {
+		toxRecords = JSON.parse(data.toString());
+	} else {
+		toxRecords = {};
+	}
+});
 
 // Handle postdata for registration
 webapp.post("/", function(req, res) {
 	console.dir(req.body);
 
-	// check if all params specified and shorten var names
-	if (!req.body.uname || !req.body.addr || !req.body.pass) return res.redirect("/errors/specifyparams.html");
+	// check if all params specified
+	if (!req.body.uname || !req.body.addr || !req.body.pass) {
+		console.log("Missing parameters!");
+		return res.redirect("/errors/specifyparams.html");
+	}
+
+	// shorten var names
 	uname = req.body.uname;
 	addr = req.body.addr;
 	pass = req.body.pass;
 
 	// Check that params are valid
-	if (uname.length >= 20 || addr.length != 76 || uname.match(/[^A-Za-z0-9_\.]/) || addr.match(/\W/)) return res.redirect("/errors/invalid.html");
+	if (uname.length >= 20 || addr.length != 76 || uname.match(/[^A-Za-z0-9_\.]/) || addr.match(/\W/)) {
+		console.log("Invalid username or password!");
+		console.log("...Username is: " + uname);
+		console.log("...Address is: " + addr);
+		return res.redirect("/errors/invalid.html");
+	}
 
-	// check to see if registered and if so, check pass
-	if (toxRecords[uname] != undefined && pass != toxRecords[uname].pass) return res.redirect("/errors/incorrect.html");
+	// check to see if registered and if so, check password
+	if (toxRecords[uname] != undefined && pass != toxRecords[uname].pass) {
+		console.log("Incorrect password, tried \"" + pass + "\", correct is \"" + toxRecords[uname].pass + "\"");
+		return res.redirect("/errors/incorrect.html");
+	};
 
+	// All good, set user and send user to success page
 	toxRecords[uname] = {
 		addr: addr,
 		pass: pass
 	};
 
+	fs.writeFileSync("./toxRecords.json", JSON.stringify(toxRecords));
 	res.redirect("/success.html");
 });
 
 // DNS listener
-dnsServer.listen(9999, "127.0.0.1", console.log("Listening..."));
+dnsServer.listen(53, "", console.log("Listening..."));
 dnsServer.on("query", function(query) {
 	var name = query.name();
 	var type = query.type();
+	var username = name.split("._tox")[0];
 
-	if (type == "TXT" && toxRecords[name]) {
-		var record = new named.TXTRecord("v=tox1;id=" + toxRecords[name].addr);
+	console.log("Request for: " + username);
+
+	if (type == "TXT" && toxRecords[username]) {
+		var record = new named.TXTRecord("v=tox1;id=" + toxRecords[username].addr);
 		query.addAnswer(name, record, 60);
-		dnsServer.send(query);
+		console.log("...Resolved to: " + toxRecords[username].addr);
+	} else {
+		console.log("...Not found :(");
 	}
+
+	dnsServer.send(query);
 });
