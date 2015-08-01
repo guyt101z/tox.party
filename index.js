faaf86e670f0bcb59b328b08a71a203340613e01
installgen2 <gen2@kek.club>
2015-08-01 06:33:47 +0100

    Added about page, added footer links, added action options on homepage (seperating edit too) and some other edits

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ webapp.listen(3000, console.log("Webapp running..."));
 webapp.use(bodyParser.json());
 webapp.use(bodyParser.urlencoded({extended: true}));
 
+webapp.set("view engine", "html");
+webapp.engine("html", function(file, data, callback) {
+	if (!data) data = {};
+
+	data.domain = config.domain;
+	data.name = config.name;
+	data.tag = config.tags[Math.floor(Math.random() * config.tags.length)];
+	data.footerLinks = config.footerLinks;;
+
+	return callback(null, swig.renderFile(file, data));
+});
+
 // Other stuff
 var fs = require("fs"),
 	passwordHash = require("password-hash"),
@@ -27,69 +39,140 @@ fs.readFile("./toxRecords.json", function(err, data) {
 	}
 });
 
-// Make serving content less ugly
-function generatePage(file, data) {
-	if (!data) data = {};
-
-	// Set some generally required data
-	data.domain = config.domain;
-	data.name = config.name;
-	data.tag = config.tags[Math.floor(Math.random() * config.tags.length)];
-
-	// Return the generated page
-	return swig.renderFile("./views/" + file + ".html", data);
-}
-
 // Static content (css, js, etc)
 webapp.use("/static", express.static("static"));
 
-// Homepage
+// Pages
 webapp.get("/", function(req, res) {
-	res.send(generatePage("index"));
+	res.render("index");
+});
+webapp.get("/about", function(req, res) {
+	res.render("about");
 });
 
 // Handle postdata for registration
 webapp.post("/", function(req, res) {
-	console.log("Recieved postdata");
+	console.log("Recieved postdata!");
 
 	// check if all params specified
 	if (!req.body.uname || !req.body.addr || !req.body.pass) {
 		console.log("Missing parameters!");
-		return res.send(generatePage("error", { error: "Missing parameters!" }));
+		return res.render("error", {
+			error: "Missing parameters!"
+		});
 	}
 
 	// shorten var names
 	var uname = req.body.uname.toLowerCase(),
+		action = req.body.action,
 		addr = req.body.addr,
 		pass = req.body.pass;
 
 	// Log some details for debugging
 	console.log("...Username is: " + uname);
+	console.log("...Action is: " + action);
 	console.log("...Address is: " + addr);
 	console.log("...IP is: " + req.ip);
 
 	// Check that params are valid
 	if (uname.length >= 20 || addr.length != 76 || uname.match(/[^A-Za-z0-9_\.]/) || addr.match(/\W/)) {
 		console.log("Invalid username or address!");
-		return res.send(generatePage("error", { error: "Invalid username or address!" }));
+		return res.render("error", {
+			error: "Invalid username or address!"
+		});
 	}
 
-	// Check to see if registered, and if so, check password
-	if (toxRecords[uname] != undefined && passwordHash.verify(pass, toxRecords[uname].pass) == false) {
-		console.log("...Input bassword hash: " + passwordHash.generate(pass));
-		console.log("...Saved bassword hash: " + toxRecords[uname].pass);
-		return res.send(generatePage("error", { error: "Incorrect password!" }));
-	};
+	switch (action) {
+	case "reg":
+		if (toxRecords[uname] != undefined) {
+			return res.render("error", {
+				error: "Account with that username already exists."
+			});
+		}
 
-	// All good, save user and send user to success page
-	toxRecords[uname] = {
-		addr: addr,
-		pass: passwordHash.generate(pass)
-	};
+		toxRecords[uname] = {
+			addr: addr,
+			pass: passwordHash.generate(pass)
+		};
+
+		res.render("success", {
+			type: action,
+			ID: uname + "@" + config.domain
+		});
+		console.log(uname + " has registered account successfully!");
+		break;
+	case "regTemp":
+		if (toxRecords[uname] != undefined) {
+			return res.render("error", {
+				error: "Account with that username already exists."
+			});
+		}
+
+		toxRecords[uname] = {
+			addr: addr,
+			pass: null
+		};
+
+		setTimeout(function() {
+			delete toxRecords[uname];
+			console.log("Deleting temporary user " + uname);
+		}, 86400000, uname);
+
+		res.render("success", {
+			type: action,
+			ID: uname + "@" + config.domain
+		});
+		console.log(uname + " has registered temporary account successfully!");
+		break;
+	case "edit":
+		if (toxRecords[uname] == undefined) {
+			return res.render("error", {
+				error: "Account with that username not found."
+			});
+		}
+
+		if (passwordHash.verify(pass, toxRecords[uname].pass) == false) {
+			return res.render("error", {
+				error: "Incorrect password!"
+			});
+		}
+
+		toxRecords[uname] = {
+			addr: addr,
+			pass: passwordHash.generate(pass)
+		};
+
+		res.render("success", {
+			type: action,
+			ID: uname + "@" + config.domain,
+			addr: addr
+		});
+		console.log(uname + " has edited account successfully!");
+		break;
+	case "del":
+		if (toxRecords[uname] == undefined) {
+			return res.render("error", {
+				error: "Account with that username not found."
+			});
+		}
+
+		if (passwordHash.verify(pass, toxRecords[uname].pass) == false) {
+			return res.render("error", {
+				error: "Incorrect password!"
+			});
+		}
+
+		delete toxRecords[uname];
+
+		console.log(uname + " has deleted account successfully!");
+		res.render("success", {
+			type: action,
+			ID: uname + "@" + config.domain
+		});
+		break;
+	}
 
 	fs.writeFileSync("./toxRecords.json", JSON.stringify(toxRecords));
-	console.log(uname + " has registered or modified ID correctly!");
-	res.send(generatePage("success", { fullId: uname + "@" + config.domain }));
 });
 
 // DNS listener
